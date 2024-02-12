312bae32899a4f62dc98c9edfbc3cf71f0959ff5
9686477769 <143290320+9686477769@users.noreply.github.com>
2024-02-12 16:35:32 +0530

    Add files via upload

---
+++ multiplicationtable.js
@@ -0,0 +1,35 @@
+const num = document.querySelector("#num");
+const btn = document.querySelector("#gen");
+const show = document.querySelector(".show");
+const times = document.querySelector(".times");
+btn.addEventListener("click",() =>{
+    let t = times.value;
+    show.innerHTML = null;
+    let n = num.value;
+    try{
+        if (n === "")
+        throw "requried field"
+        
+    }
+    catch(error){
+        show.innerHTML=error;  
+    }
+    
+
+    try{
+        if(t === "")
+        throw "required field"
+   
+    }
+    catch(error){
+        show.innerHTML=error;
+    }
+   
+
+    for(let i=1;i<=t;i++){
+        let a = `${n} x ${i} = ${n*i}<br>`  
+        show.innerHTML+= a;
+
+    
+    }
+});
